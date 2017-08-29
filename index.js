80bc56275042faf9aae80a63d670ffce745967c5
Sara Hopper <sara.hopper@gmail.com>
2017-08-29 19:14:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,10 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var sortedRobots = robots.map(function (robots)) {
+  return Object.assign({}, robot, {
+    if 
+    alliance: 
+  })
+}
